cd06e3a13d8998da332982dee374f26c0ef8f210
Sijin Park <sijinpark77@gmail.com>
2024-12-17 19:30:50 +0900

    feat: clorox implememt

--- api/clorox.py
+++ api/clorox.py
@@ -3,25 +3,36 @@ from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
 def crawl():
-    url = "https://www.coupang.com/np/search?component=&q=%EC%9C%A0%ED%95%9C%EB%9D%BD%EC%8A%A4&channel=user"
+    try:
+        url = "https://yuhanrox.co.kr/Shop/16831"
 
-    headers = {
-        "User-Agent": UserAgent().random,
-        "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
-    }
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.86 Safari/537.36",
+            "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
+        }
+        print(headers)
 
-    response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers)
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    results = soup.find_all('em')
-    print(results)
-    return {
-		"template": "big-number",
-		"number": "1300",
-		"unit": "원/100mL",
-		"image": "clorox"
-	}
+        results = soup.find(id="sales_price")
+
+        price = results.get_text()
+        # print(results.get_text())
+        return {
+            "template": "big-number",
+            "number": str(int(price) // 20),
+            "unit": "원/100mL",
+            "image": "clorox"
+        }
+    except:
+        return {
+            "template": "big-number",
+            "number": "210",
+            "unit": "원/100mL",
+            "image": "clorox"
+        }
 
 if __name__ == "__main__":
     print(crawl())
--- api/index.py
+++ api/index.py
@@ -1,41 +1,11 @@
 from flask import Flask, jsonify
-from fake_useragent import UserAgent
-import requests
-from bs4 import BeautifulSoup
+# from fake_useragent import UserAgent
+# import requests
+# from bs4 import BeautifulSoup
 import datetime
 
-def hangang():
-    url = "https://hangang.life/"
-
-    response = requests.get(url)
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    results = soup.find_all('div', {"class": "fullscreen"})
-
-    return {"unit": "°C", "number":results[0].find("span").string.split("|")[1].split("°")[0], "template": "big-number","image":"hangang",}
-
-
-def clorox():
-    # url = "https://www.coupang.com/np/search?component=&q=%EC%9C%A0%ED%95%9C%EB%9D%BD%EC%8A%A4&channel=user"
-
-    # headers = {
-    #     "User-Agent": UserAgent().random,
-    #     "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
-    # }
-
-    # response = requests.get(url, headers=headers)
-
-    # soup = BeautifulSoup(response.text, 'html.parser')
-
-    # results = soup.find_all('em')
-    # print(results)
-    return {
-		"template": "big-number",
-		"number": "1300",
-		"unit": "원/100mL",
-		"image": "clorox"
-	}
+import hangang
+import clorox
 
 app = Flask(__name__)
 
@@ -45,8 +15,8 @@ def home():
         now = datetime.datetime.now() + datetime.timedelta(hours=9)
         crawls = [{"template": "clock","date": now.strftime("%y/%m/%d(%a)"), "time": now.strftime("%H:%M")}]
 
-        crawls.append(hangang())
-        crawls.append(clorox())
+        crawls.append(hangang.crawl())
+        crawls.append(clorox.crawl())
 
         return jsonify(crawls), 200
     
