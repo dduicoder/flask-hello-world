d370f57a436395d17bdf3246ffe53a4e2e8c22a3
Sijin Park <sijinpark77@gmail.com>
2024-12-17 19:50:04 +0900

    feat: implement files

--- api/index.py
+++ api/index.py
@@ -1,11 +1,51 @@
 from flask import Flask, jsonify
-# from fake_useragent import UserAgent
-# import requests
-# from bs4 import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
 import datetime
+import requests
 
-from . import hangang
-from . import clorox
+def hangang():
+    url = "https://hangang.life/"
+
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    results = soup.find_all('div', {"class": "fullscreen"})
+
+    return {"unit": "\u00b0C", "number":results[0].find("span").string.split("|")[1].split("°")[0], "template": "big-number",}
+
+def clorox():
+    try:
+        url = "https://yuhanrox.co.kr/Shop/16831"
+
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.86 Safari/537.36",
+            "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
+        }
+        print(headers)
+
+        response = requests.get(url, headers=headers)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        results = soup.find(id="sales_price")
+
+        price = results.get_text()
+        # print(results.get_text())
+        return {
+            "template": "big-number",
+            "number": str(int(price) // 20),
+            "unit": "원/100mL",
+            "image": "clorox"
+        }
+    except:
+        return {
+            "template": "big-number",
+            "number": "210",
+            "unit": "원/100mL",
+            "image": "clorox"
+        }
 
 app = Flask(__name__)
 
@@ -15,8 +55,8 @@ def home():
         now = datetime.datetime.now() + datetime.timedelta(hours=9)
         crawls = [{"template": "clock","date": now.strftime("%y/%m/%d(%a)"), "time": now.strftime("%H:%M")}]
 
-        crawls.append(hangang.crawl())
-        crawls.append(clorox.crawl())
+        crawls.append(hangang())
+        crawls.append(clorox())
 
         return jsonify(crawls), 200
     
